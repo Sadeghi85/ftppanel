7321d2f6bf48b99fce4a6d94fbd9ba3d284e57c6
sadeghi85 <sadeghi85@hotmail.com>
2014-05-20 07:29:49 -0400

    .

---
+++ cmd/uploadscript.php
@@ -0,0 +1,27 @@
+#!/opt/webpanel/lamp/php/bin/php
+<?php
+
+$ftpHome = '/var/www/FtpPanel/cdn1';
+$cdnDomain = 'cdn1.iribtv.ir';
+
+$dir = str_replace($ftpHome, '', $argv[1]);
+
+file_put_contents($argv[1].'.txt', 'http://'.$cdnDomain.encodeURI($dir));
+
+function encodeURI($url) {
+    // http://php.net/manual/en/function.rawurlencode.php
+    // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/encodeURI
+    $unescaped = array(
+        '%2D'=>'-','%5F'=>'_','%2E'=>'.','%21'=>'!', '%7E'=>'~',
+        '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')'
+    );
+    $reserved = array(
+        '%3B'=>';','%2C'=>',','%2F'=>'/','%3F'=>'?','%3A'=>':',
+        '%40'=>'@','%26'=>'&','%3D'=>'=','%2B'=>'+','%24'=>'$'
+    );
+    $score = array(
+        '%23'=>'#'
+    );
+    return strtr(rawurlencode($url), array_merge($reserved,$unescaped,$score));
+
+}
